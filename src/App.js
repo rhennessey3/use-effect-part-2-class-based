ec362468954635ac22ee8d383bbdca29fc720dbb
Richard Hennessey III <Rick@Richards-iMac.local>
2020-06-19 13:52:04 -0400

    use effect finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
+import randomcolor from "randomcolor"
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("")
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      // setCount(prevCount => prevCount + 1)
+    }, 1000)
+    return () => clearInterval(intervalId)
+  }, [])
+
+  useEffect(() => {
+    setColor(randomcolor())
+  }, [count])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{ color: color }}>{count}</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
